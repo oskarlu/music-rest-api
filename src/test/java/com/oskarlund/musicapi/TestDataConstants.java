77fa64e389610ba8287da6106910a56662440527
Oskar Lund <oskar@sevenfeet.se>
2023-12-03 21:19:00 +0100

    fix due to upgraded deps

--- src/test/java/com/oskarlund/musicapi/TestDataConstants.java
+++ src/test/java/com/oskarlund/musicapi/TestDataConstants.java
@@ -8,9 +8,11 @@ import com.oskarlund.musicapi.musicbrainz.MBRelations;
 import com.oskarlund.musicapi.musicbrainz.MBReleaseGroup;
 import feign.FeignException;
 import feign.Request;
+import feign.RequestTemplate;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 
 public class TestDataConstants {
@@ -18,8 +20,16 @@ public class TestDataConstants {
 	public static final String NIRVANA_MBID = "5b11f4ce-a62d-471e-81fc-a69a8278c7da";
 	public static final String MJ_MBID = "f27ec8db-af05-4f36-916e-3d57f91ecf5e";
 
-	public static final FeignException.NotFound MB_NOT_FOUND = new FeignException.NotFound("Not found in MusicBrainz", Request.create(Request.HttpMethod.GET, "the url", Collections.singletonMap("", Arrays.asList("")), Request.Body.empty()), null);
-	public static final FeignException.BadRequest MB_BAD_REQUEST = new FeignException.BadRequest("MusicBrainz says Bad request", Request.create(Request.HttpMethod.GET, "the url", Collections.singletonMap("", Arrays.asList("")), Request.Body.empty()), null);
+	public static final FeignException.NotFound MB_NOT_FOUND = new FeignException.NotFound(
+			"Not found in MusicBrainz",
+//			Request.create(Request.HttpMethod.GET, "the url", Collections.singletonMap("", List.of("")), Request.Body.empty()), // OLD
+			Request.create(Request.HttpMethod.GET, "the url", Collections.singletonMap("", List.of("")), Request.Body.empty(), new RequestTemplate()), // NEW
+			null);
+	public static final FeignException.BadRequest MB_BAD_REQUEST = new FeignException.BadRequest(
+			"MusicBrainz says Bad request",
+//			Request.create(Request.HttpMethod.GET, "the url", Collections.singletonMap("", List.of("")), Request.Body.empty()), // OLD
+			Request.create(Request.HttpMethod.GET, "the url", Collections.singletonMap("", List.of("")), Request.Body.empty(), new RequestTemplate()), // NEW
+			null);
 
 	public static final String NEVERMIND_CAA_ID = "1b022e01-4da6-387b-8658-8678046e4cef";
 	public static final String THE_VERY_BEST_CAA_ID = "5bcaeba6-a532-3fa0-b540-75c09f70f759";
