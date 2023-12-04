811c070bec46f9aba84a5e6334eefa1837781ef9
Oskar Lund <oskar@sevenfeet.se>
2023-12-04 13:45:58 +0100

    migrate from Swagger2 -> 3 (springdoc)

--- src/main/java/com/oskarlund/musicapi/MusicApiApplication.java
+++ src/main/java/com/oskarlund/musicapi/MusicApiApplication.java
@@ -7,17 +7,17 @@ import com.oskarlund.musicapi.discogs.DiscogsDescriptionManager;
 import com.oskarlund.musicapi.musicbrainz.MusicBrainzClient;
 import com.oskarlund.musicapi.musicbrainz.MusicBrainzManagerImpl;
 import feign.Logger;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableSwagger2
+@OpenAPIDefinition
 @Configuration
 public class MusicApiApplication {
 
--- src/main/java/com/oskarlund/musicapi/config/SwaggerConfig.java
+++ src/main/java/com/oskarlund/musicapi/config/SwaggerConfig.java
@@ -1,18 +1,19 @@
 package com.oskarlund.musicapi.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springdoc.core.Constants.CLASSPATH_RESOURCE_LOCATION;
 
 @Configuration
-public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.oskarlund.musicapi.web"))
-                .build();
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATION);
     }
 }
