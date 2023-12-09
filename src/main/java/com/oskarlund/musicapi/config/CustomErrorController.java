8eb4b350c4a00584747d60305eb10590b80eb6da
Oskar Lund <oskar@sevenfeet.se>
2023-12-09 20:39:51 +0100

    impl custom error page

---
+++ src/main/java/com/oskarlund/musicapi/config/CustomErrorController.java
@@ -0,0 +1,23 @@
+package com.oskarlund.musicapi.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomErrorController {
+
+    public static final Logger LOG = LoggerFactory.getLogger(CustomErrorController.class);
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+        LOG.error("Exception thrown: {}", ex.getClass().getSimpleName(), ex);
+
+        model.addAttribute("status", 500);
+        model.addAttribute("error", ex.getClass().getSimpleName());
+        model.addAttribute("message", ex.getMessage() != null ? ex.getMessage() : "null msg");
+        return "error/error";
+    }
+}
